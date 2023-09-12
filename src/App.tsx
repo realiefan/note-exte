284096062ac33bcb780ed220490739c3831203c1
Iefan <66840141+realiefan@users.noreply.github.com>
2023-09-13 05:26:26 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -109,7 +109,10 @@ function App() {
   return (
     <div className="app">
       <div className="flex flex-col  gap-16">
-        <h1 className="text-lg font-semibold ">Quick Notes by NostrNet.work</h1>
+        <h1 className="text-lg font-bold ">Quick Notes by{" "}
+        <a href="https://www.nostrnet.work"  rel="noopener noreferrer">
+          NostrNet
+        </a></h1>
         <CreateNote pool={pool} hashtags={hashtags} />
 
       </div>
